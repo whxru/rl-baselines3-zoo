71ab11d81f6143cf8d6072d24c66f39dee7245b8
whxru <seuhuangziyao@outlook.com>
2022-12-06 16:53:05 +0800

    Adjust the searching range

--- rl_zoo3/hyperparams_opt.py
+++ rl_zoo3/hyperparams_opt.py
@@ -121,9 +121,8 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     # batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128, 256, 512, 1024, 2048])
     # batch_size = trial.suggest_categorical("batch_size", [64, 256, 512, 1024, 4096, 6144, 8192])
     # n_steps = trial.suggest_categorical("n_steps", [64, 256, 512, 1024, 4096, 6144, 8192, 10240, 14336])
-    # n_steps = trial.suggest_categorical("n_steps", [16, 32, 64, 128, 256, 512, 1024, 2048])
-    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128, 256, 512])
-    n_steps = trial.suggest_categorical("n_steps", [512])
+    n_steps = trial.suggest_categorical("n_steps", [16, 32, 64, 128, 256, 512, 1024, 2048])
+    batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128, 256, 512, 1024, 2048])
     gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
     learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1)
     lr_schedule = "constant"
